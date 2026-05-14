4795e626eb06a93dfc6bcad215890177412a70e4
Jafar610 <jafarmohamed955@gmail.com>
2026-05-14 04:02:21 +0300

    initail commit

---
+++ frontend/src/Components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <>
+        <h1 className='text-3xl font-bold bg-gray-100 text-gray-800'>Navbar</h1>
+    </>
+  )
+}
+
+export default Navbar
