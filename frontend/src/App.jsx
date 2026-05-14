49b4d2bfa2cb012a5d52a88dd42609876d1af7f0
Jafar610 <jafarmohamed955@gmail.com>
2026-05-14 15:24:51 +0300

    2 commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
+import Home from './Components/Home'
 
 function App() {
 
   return (
     <>
-      <h1 className='text-3xl font-bold bg-gray-100 text-gray-800'>Full Netflix Clone</h1>
+      <Home/>
     </>
   )
 }
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
-
+import logo from '../assets/Netflix_logo.png'
+import profile from '../assets/Netflix-avatar.png'
 function Navbar() {
   return (
     <>
-        <h1 className='text-3xl font-bold bg-gray-100 text-gray-800'>Navbar</h1>
+        <nav className='bg-[#111] text-white flex justify-between items-center p-3'>
+            <div className='flex justify-center items-center'>
+                <img src={logo} alt="" className='w-25 mr-10' />
+                <ul className='flex justify-center items-center gap-5 text-gray-100'>
+                    <li>Home</li>
+                    <li>Tv Show</li>
+                    <li>Movie</li>
+                    <li>News & Popular</li>
+                    <li>My List</li>
+                </ul>
+            </div>
+
+
+            <div className='flex justify-center items-center gap-4'>
+                <img src={profile} alt="" className='w-8 rounded-lg'/>
+                <button className='border border-[#E50914] px-3 py-1 rounded-md bg-[#E50914] text-white font-meduim'>Logout</button>
+            </div>
+        </nav>
     </>
   )
 }
