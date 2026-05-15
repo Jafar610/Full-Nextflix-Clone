26bcb7c4377596f24cacbe4544fd07c719e572b4
Jafar610 <jafarmohamed955@gmail.com>
2026-05-15 07:28:50 +0300

    2nd commit

--- frontend/src/Components/Hero.jsx
+++ frontend/src/Components/Hero.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
-
+import React from "react";
+import PlayCircleIcon from "@mui/icons-material/PlayCircle";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import InfoIcon from "@mui/icons-material/Info";
 function Hero() {
   return (
-    <div>Hero</div>
-  )
+    <>
+      <div
+        className="w-full h-[500px] bg-cover bg-center relative"
+        style={{
+          backgroundImage:
+            "url('https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg')",
+        }}
+      >
+        <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-t from-[#111] to-transparent"></div>
+        <div className="absolute bottom-10 left-10 text-white">
+          <h1 className="text-4xl font-bold mb-4 ">YOU</h1>
+          <p className="text-lg mb-6 w-150">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi
+            praesentium ea aliquid voluptatem sapiente a odio nihil explicabo,
+            temporibus qui.
+          </p>
+          <div className="flex justify-start items-center gap-3">
+            <div className="flex justify-center items-center gap-2 border border-[#E50914] px-5 py-2 rounded-md bg-[#E50914] text-white font-medium mr-5 cursor-pointer">
+              <PlayCircleIcon className="text-4xl text-white mr-2" />
+              <span className="text-1xl font-medium">Play</span>
+            </div>
+
+            <div className="flex justify-center items-center gap-2 border border-gray-300 px-5 py-2 rounded-md bg-transparent text-gray-300 font-medium cursor-pointer">
+              <AddCircleIcon className="text-5xl text-white mr-2 cursor-pointer" />
+              <span className="text-1xl text-white font-medium cursor-pointer">
+                My List
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import logo from '../assets/Netflix_logo.png'
 import profile from '../assets/Netflix-avatar.png'
+import SearchIcon from '@mui/icons-material/Search';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 function Navbar() {
   return (
     <>
-        <nav className='bg-[#111] text-white flex justify-between items-center p-3'>
+        <nav className='bg-[#111]  text-white flex justify-between items-center p-3'>
             <div className='flex justify-center items-center'>
                 <img src={logo} alt="" className='w-25 mr-10' />
                 <ul className='flex justify-center items-center gap-5 text-gray-100'>
@@ -18,8 +20,11 @@ function Navbar() {
 
 
             <div className='flex justify-center items-center gap-4'>
-                <img src={profile} alt="" className='w-8 rounded-lg'/>
-                <button className='border border-[#E50914] px-3 py-1 rounded-md bg-[#E50914] text-white font-meduim'>Logout</button>
+                <SearchIcon className='text-white cursor-pointer'/>
+                <div className='flex justify-center items-center gap-2 cursor-pointer'>
+                    <img src={profile} alt="" className='w-8 rounded-full'/>
+                    <ArrowDropDownIcon className='text-white'/>
+                </div>
             </div>
         </nav>
     </>
