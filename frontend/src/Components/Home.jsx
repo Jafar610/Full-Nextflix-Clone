1c46d68a62aa3824c779cb69ecff7c16d8533d86
Jafar610 <jafarmohamed955@gmail.com>
2026-05-16 21:04:44 +0300

    3rd commit

--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
+import MovieCard from './MovieCard'
 
 function Home() {
   return (
     <>
     <Navbar/>
     <Hero/>
+    <MovieCard/>
     </>
   )
 }
--- frontend/src/Components/MovieCard.jsx
+++ frontend/src/Components/MovieCard.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
-
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+// import {autoplay} from "swiper";
 function MovieCard() {
   return (
-    <div>MovieCard</div>
-  )
+    <>
+      <div className="w-[200px] h-[300px] bg-cover bg-center relative rounded-md cursor-pointer">
+        <h2>Movie Categories</h2>
+        <div>
+          <div>
+            <img
+              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
+              alt=""
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+          <div>
+            <img
+              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
+              alt=""
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default MovieCard
+export default MovieCard;
