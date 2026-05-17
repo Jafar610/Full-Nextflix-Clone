9e21060272d935dcf86e7c1345e1f1761cffd8c2
Jafar610 <jafarmohamed955@gmail.com>
2026-05-17 22:01:44 +0300

    3rd commit

--- frontend/src/Components/MovieCard.jsx
+++ frontend/src/Components/MovieCard.jsx
@@ -1,29 +1,10 @@
 import React from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
+import Row from "./Row";
 // import {autoplay} from "swiper";
 function MovieCard() {
   return (
     <>
-      <div className="w-[200px] h-[300px] bg-cover bg-center relative rounded-md cursor-pointer">
-        <h2>Movie Categories</h2>
-        <div className='flex justify-start items-center gap-2'>
-          <div>
-            <img
-              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
-              alt=""
-              className="w-full h-full object-cover rounded-md"
-            />
-          </div>
-          <div>
-            <img
-              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
-              alt=""
-              className="w-full h-full object-cover rounded-md"
-            />
-          </div>
-        </div>
-      </div>
+     <Row/>
     </>
   );
 }
--- frontend/src/Components/Row.jsx
+++ frontend/src/Components/Row.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 function Row() {
   return (
     <>
-    
+       <div className="w-[300px] h-[400px] bg-cover bg-center relative rounded-md cursor-pointer m-3">
+        <h2>Movie Categories</h2>
+        <div className='flex justify-start items-center gap-4 mt-3'>
+          <div>
+            <img
+              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
+              alt=""
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+          <div>
+            <img
+              src="https://i.pinimg.com/1200x/93/bc/eb/93bceb9dc583c4511b9742b182b763aa.jpg"
+              alt=""
+              className="w-full h-full object-cover rounded-md"
+            />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
