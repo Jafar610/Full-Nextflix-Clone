6df50668c7b5e0f8d8b199ef58d70288e216debc
Jafar610 <jafarmohamed955@gmail.com>
2026-05-19 17:20:34 +0300

    4th commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Components/Home'
+import Detail from './Pages/Detail'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <Router>
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/detail' element={<Detail />} />
         </Routes>
       </Router>
     </>
--- frontend/src/Pages/Detail.jsx
+++ frontend/src/Pages/Detail.jsx
@@ -1,58 +1,62 @@
-import React from 'react'
-import StarIcon from '@mui/icons-material/Star';
+import React from "react";
+import StarIcon from "@mui/icons-material/Star";
 function Detail() {
   return (
     <>
-        <div className='flex justify-center items-center w-full h-screen'>
-            {/* image */}
-            <div className=''>
-                <img src="" alt="" className='' />
-            </div>
-            {/* Detail Info */}
-            <div>
-                {/* Movie title and Star */}
-                <div>
-                    <h1>Movie Title</h1>
-                    <div>
-                        <p>9.0</p>
-                        <StarIcon/>
-                    </div>
-                </div>
-                {/* movie date & movie time length */}
-                <div>
-                    <h1>2018 | 2h 30m | 16+</h1>
-                </div>
-
-                {/* Navigation  */}
-                <div>
-                    <ul>
-                        <li>Overview</li>
-                        <li>Trailers & More</li>
-                        <li>More Like this</li>
-                        <li>Detail Info</li>
-                    </ul>
-                </div>
-
-                {/* Detail Info */}
-
-                <div>
-
-                </div>
-
-                {/* Related Movies */}
-                <div>
-                    <h1>Related Movies</h1>
-                    <div className='flex justify-center items-center overflow-x overflow-hidden'>
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                        <img src="" alt="" />
-                    </div>
-                </div>
-            </div>
+      <div className="flex justify-between items-center w-full h-screen bg-[#111111] text-white px-15">
+        {/* image */}
+        <div className="w-100 h-full flex justify-center items-center bg-red-500">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlRNrj_ilBOh0nfhXRZLd4mbINGj4qEEeZFQ&s"
+            alt=""
+            className="w-80 rounded-lg"
+          />
         </div>
+        {/* Detail Info */}
+        <div className="w-full h-full flex justify-center items-center bg-blue-500">
+          <div>
+            {/* Movie title and Star */}
+            <div>
+              <h1>Movie Title</h1>
+              <div>
+                <p>9.0</p>
+                <StarIcon />
+              </div>
+            </div>
+            {/* movie date & movie time length */}
+            <div>
+              <h1>2018 | 2h 30m | 16+</h1>
+            </div>
+
+            {/* Navigation  */}
+            <div>
+              <ul>
+                <li>Overview</li>
+                <li>Trailers & More</li>
+                <li>More Like this</li>
+                <li>Detail Info</li>
+              </ul>
+            </div>
+
+            {/* Detail Info */}
+
+            <div></div>
+
+            {/* Related Movies */}
+            <div>
+              <h1>Related Movies</h1>
+              <div className="flex justify-center items-center overflow-x overflow-hidden">
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+                <img src="" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default Detail
+export default Detail;
