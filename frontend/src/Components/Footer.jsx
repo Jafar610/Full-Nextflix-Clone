9c4a493257461e1d1ee6ca480a0185a40c8a89de
Jafar610 <jafarmohamed955@gmail.com>
2026-05-23 22:59:00 +0300

    file updated

--- frontend/src/Components/Footer.jsx
+++ frontend/src/Components/Footer.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
-
+import logo from '../assets/Netflix_logo.png'
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+      <div className='flex justify-between items-center bg-[#111] '>
+        <div>
+            <img src={logo} alt="netflix logo" className='w-20' />
+            
+        </div>
+        <div>
+            <ul className='text-white'>
+              <li>Home</li>
+              <li>About</li>
+              <li>Contact</li>
+              <li>Tv shows</li>
+            </ul>
+        </div>
+        <div>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
 
--- frontend/src/Components/Home.jsx
+++ frontend/src/Components/Home.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import Navbar from './Navbar'
 import Hero from './Hero'
 import MovieCard from './MovieCard'
+import Footer from './Footer'
 
 function Home() {
   return (
     <>
     <Navbar/>
-    <Hero/>
-    <MovieCard/>
+    {/* <Hero/>
+    <MovieCard/> */}
+
+    <Footer/>
     </>
   )
 }
